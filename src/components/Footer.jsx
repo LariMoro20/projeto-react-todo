8ec20f323d3b623b32a20ac1ef74c8ed2735b203
Larissa Moro <54962550+LariMoro20@users.noreply.github.com>
2021-12-25 21:37:37 -0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ const year = new Date().getFullYear();
 const Footer = () => {
   return (
     <footer className="section_footer">
-      Desenvolvido no <br /><a target="_blank" rel="noreferrer" href="https://www.youtube.com/watch?v=ErjWNvP6mko">"Curso de React Para Completos Iniciantes" - DicasParaDevs </a>
+      Desenvolvido no <br /><a style={{ textDecoration: "none", color: "white" }} target="_blank" rel="noreferrer" href="https://www.youtube.com/watch?v=ErjWNvP6mko">"Curso de React Para Completos Iniciantes" - DicasParaDevs </a>
       <a target="_blank" rel="noreferrer"
         href="https://github.com/LariMoro20/projeto-react-todo"
         style={{ textDecoration: "none", color: "white" }}
