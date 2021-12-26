b523b4badde4f643131abc61566e2737e8c9c4ec
Larissa Moro <54962550+LariMoro20@users.noreply.github.com>
2021-12-25 21:37:06 -0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,10 +3,11 @@ const year = new Date().getFullYear();
 const Footer = () => {
   return (
     <footer className="section_footer">
-      <a
+      Desenvolvido no <br /><a target="_blank" rel="noreferrer" href="https://www.youtube.com/watch?v=ErjWNvP6mko">"Curso de React Para Completos Iniciantes" - DicasParaDevs </a>
+      <a target="_blank" rel="noreferrer"
         href="https://github.com/LariMoro20/projeto-react-todo"
         style={{ textDecoration: "none", color: "white" }}
-      >
+      ><br></br>
         {year} &copy; Larissa Santos
         <img
           src="https://image.flaticon.com/icons/png/512/25/25231.png"
@@ -15,7 +16,7 @@ const Footer = () => {
           style={{ marginLeft: "10px" }}
         />
       </a>
-    </footer>
+    </footer >
   );
 };
 
